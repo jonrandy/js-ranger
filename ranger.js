1813cf3fa9d5406b410b462e445ee217fd140249
Jon Randy <jonrandy@gmail.com>
2023-04-06 15:47:36 +0700

    WIP

--- ranger.js
+++ ranger.js
@@ -1,7 +1,32 @@
-export function attach(object, rangeFunc) {
+const RANGER_SYMBOL = Symbol('ranger')
+
+let isInitialised = false
+
+export function attach(target, rangeFunc) {
+
+  !isInitialised && init()
 	
 }
 
 export function initNumberRangeSyntax() {
 	
+}
+
+
+function init() {
+
+  // set up our 'toPrimitive' method on the Array prototype
+  Array.prototype[Symbol.toPrimitive] = function (hint) {
+    // try to leave as much default behaviour alone as possible
+    if (hint === 'default') return this.toString()
+    if (hint === 'number') return Number(this.toString())
+    if (!isValidRangerArray(this)) return this.toString()
+    // return the symbol that 'names' the method on the target object
+    return this[2]
+  }
+
+}
+
+function isValidRangerArray(arr) {
+
 }
